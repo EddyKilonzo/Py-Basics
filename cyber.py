7f325b33233d64d1a08bc76719761feca37edefd
Eddy Kilonzo <eddymax3715@gmail.com>
2024-10-16 09:58:29 +0300

    py basics

--- cyber.py
+++ cyber.py
@@ -6,7 +6,7 @@
 
  #varibles
 
-device_ID = "EB3/56398"
+# device_ID = "EB3/56398"
 
 # print (device_ID)
 
@@ -49,14 +49,70 @@ device_ID = "EB3/56398"
 
 #retun info from a fuction 
 
-def caclulate_fails(total_attempts, failed_attempts):
-    fail_percentage = failed_attempts / total_attempts
+# def caclulate_fails(total_attempts, failed_attempts):
+#     fail_percentage = failed_attempts / total_attempts
 
-    return fail_percentage
+#     return fail_percentage
 
-percentage = caclulate_fails(4, 2)
+# percentage = caclulate_fails(4, 2)
 
 
-if (percentage >= .5):
-    print("account locked")
+# if (percentage >= .5):
+#     print("account locked")
+
+
+#Working with strongs
+
+# str() -> converts input into a string
+
+# new_string = str(123)
+
+# print(type(new_string))
+
+# len() -> returns number of objects in an element
+
+# print(len("hello"))
+
+#String concatenaton (+)
+
+# print("Hello" + "World!")
+
+
+#methods
+# print("hello".upper())
+# print("hello".lower())
+
+# .index() finds the first occurence of the input in a string and returns its location
+
+# print("Hello" .index("e"))
+
+#lists
+
+# my_list = ["a", "b", "c","d", "e"]
+# print(my_list[3])
+
+# list concatenation
+# number_list = ["1", "2", "3", "4"]
+# print(my_list + number_list)
+
+# my_list.insert(1, 7)
+
+# address = "198.567.xxx.xxx"
+# print(address [0:3])
+
+# import re 
+
+# email_log = """ Email received June 2 from user1@gmail.com.
+#             Email received June 2 from user90@gmail.com.
+#             Email received June 2 from user2@gmail.com.  """
+
+# print(re.findall("\w+@+\w+\.+\w+", email_log))
+            
+
+#opening text file
+
+# with open("login_attempts.txt", "r") as file :
+#     file_text = file.read()
+# print(file.text)   
+
 
